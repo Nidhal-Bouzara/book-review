40145d73f0702b9e4b21b497266b2a1197c142d9
Nidhal Anis BOUZARA <nidhalanis.bouzara@gmail.com>
2024-04-01 00:22:27 +0200

    redploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,11 @@ import { books } from "../data/books";
 import Navigate from "./navigation";
 import styles from './page.module.css';
 
-const getRandomSubArray = (arr: any[], size: number) => {
-  const shuffled = arr.sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, size);
-}
-
 export default async function Home() {
+  const getRandomSubArray = (arr: any[], size: number) => {
+    const shuffled = arr.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, size);
+  }
   // get random sub array from array
   const books = await fetchFromGoogleBooks()
   return (
